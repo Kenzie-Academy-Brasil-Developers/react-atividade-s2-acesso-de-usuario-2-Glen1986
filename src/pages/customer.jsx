02f1d792517fc88524f3bfe6ca350c3a700c5e2f
glen1986 <escobar.glen@gmail.com>
2021-09-15 12:05:15 -0300

    renderName

--- src/pages/customer.jsx
+++ src/pages/customer.jsx
@@ -1,34 +1,23 @@
 import React from 'react';
-import { 
-  useParams, 
-  Link, 
-  useHistory 
-} from 'react-router-dom';
-/***
-  *const param = useParams();
-  const member = members.find(item =>param)
-  console.log(history)
-  const history = useHistory()
-  * ****/
+import { useParams, Link } from 'react-router-dom';
+
 const Customer =({members})=>{
   
-
+  const params = useParams();
+  const param = params["id"]
+  const member = members.find(item => item.id === param)
+ 
     return(
 
 <div>
   <h1>Detalhes do cliente</h1>
-
   <p>
-    Nome: customer{}
-
+    Nome: { member && member.name }
   </p>
-
   <Link to="/">
     Voltar
   </Link>
 </div>
-
-
   )
 }
 export default Customer;
